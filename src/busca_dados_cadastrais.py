0363cffd57667e405c0bdef561b80eb8af801765
Leonardo Lopes Lessa da Fonseca <leonardo.lessa@materacapital.com.br>
2022-12-01 00:14:38 -0300

    fix

--- src/busca_dados_cadastrais.py
+++ src/busca_dados_cadastrais.py
@@ -42,4 +42,4 @@ class BuscaDadosCadastrais(BuscaDados):
 
         if return_df.empty: return Response("", 204, mimetype="text/plain")
 
-        return Response(dumps(return_df.to_dict("records")[0]), 200, mimetype="application/json")
+        return Response(dumps(return_df.values.tolist()), 200, mimetype="application/json")
