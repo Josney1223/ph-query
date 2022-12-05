3194f0e8543fb55ca022f200fa1a30c3d0f320de
Leonardo Lopes Lessa da Fonseca <leonardo.lessa@materacapital.com.br>
2022-12-05 19:13:26 -0300

    fix

--- src/scripts/analise_cliente.py
+++ src/scripts/analise_cliente.py
@@ -16,4 +16,4 @@ class AnaliseCliente(BuscaDados):
 
         if return_df.empty: return Response("", 204, mimetype="text/plain")
 
-        return Response(dumps(return_df.to_dict("records")[0], default=str), 200, mimetype="application/json")
+        return Response(dumps(return_df.to_dict("records"), default=str), 200, mimetype="application/json")
