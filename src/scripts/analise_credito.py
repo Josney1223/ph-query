fd9151a8e5a35f980e3c6bc5eaa1e2bf4a63f665
Leonardo Lopes Lessa da Fonseca <leonardo.lessa@materacapital.com.br>
2022-12-04 22:11:48 -0300

    fix busca assessor

--- src/scripts/analise_credito.py
+++ src/scripts/analise_credito.py
@@ -4,12 +4,13 @@ from json import dumps
 from flask import Response
 from src.classes.busca_dados import BuscaDados
 
-class AnaliseCliente(BuscaDados):
-    def AnaliseCliente(self, token: str, id_user: str) -> Response:
+class AnaliseAssessor(BuscaDados):
+    def analise_assessor(self, token: str) -> Response:
         # Busca id
         resp_token: Response = self._validar_token(token)        
         if resp_token.status_code != 200: return Response(resp_token.content, resp_token.status_code)
-    
+        id_user: int = resp_token.json()["id_user"]
+        
         # Query
         return_df: pd.DataFrame = self._run_query("CALL AnaliseCreditoPorAAI({});".format(id_user))
 
