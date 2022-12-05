92a4d973fc0e9b79c44670d93d7cff7dde4a610b
Leonardo Lopes Lessa da Fonseca <leonardo.lessa@materacapital.com.br>
2022-12-04 22:18:28 -0300

    fix

--- src/scripts/analise_cliente.py
+++ src/scripts/analise_cliente.py
@@ -5,7 +5,7 @@ from flask import Response
 from src.classes.busca_dados import BuscaDados
 
 class AnaliseCliente(BuscaDados):
-    def AnaliseCliente(self, token: str, id_user: str) -> Response:
+    def analise_cliente(self, token: str, id_user: str) -> Response:
         # Busca id
         resp_token: Response = self._validar_token(token)        
         if resp_token.status_code != 200: return Response(resp_token.content, resp_token.status_code)
@@ -14,5 +14,5 @@ class AnaliseCliente(BuscaDados):
         return_df: pd.DataFrame = self._run_query("CALL AnaliseCreditoPorAAI({});".format(id_user))
 
         if return_df.empty: return Response("", 204, mimetype="text/plain")
-        
+
         return Response(dumps(return_df.to_dict("records")[0], default=str), 200, mimetype="application/json")
