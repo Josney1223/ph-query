d4437959c5622989ac21d1d0d6f3ee5974c05a7d
Leonardo Lopes Lessa da Fonseca <leonardo.lessa@materacapital.com.br>
2022-12-07 21:04:06 -0300

    Fix

--- src/scripts/sistema_arquivos.py
+++ src/scripts/sistema_arquivos.py
@@ -13,10 +13,14 @@ class SistemaArquivos(BuscaDados):
         return self._run_query("CALL BuscaCPF({});".format(id)).values.tolist()[0][0]
     
     def fazer_request(self, request: Request) -> Response:                
+        
+        if "UserID" not in request.args.keys():
         # Busca id
-        resp_token: Response = self._validar_token(request.headers.get('Access-Token'))        
-        if resp_token.status_code != 200: return Response(resp_token.content, resp_token.status_code)
-        id_user: int = resp_token.json()["id_user"]
+            resp_token: Response = self._validar_token(request.headers.get('Access-Token'))        
+            if resp_token.status_code != 200: return Response(resp_token.content, resp_token.status_code)
+            id_user: int = resp_token.json()["id_user"]
+        else:        
+            id_user: int = request.args.get("UserID")
 
         cpf: str = self._buscar_cpf_cadastro(id_user)
 
